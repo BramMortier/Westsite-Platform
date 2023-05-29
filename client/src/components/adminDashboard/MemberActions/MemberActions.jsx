6b41578248820dd555d48de0a3795012c1081d97
BramMortier <79015250+BramMortier@users.noreply.github.com>
2023-05-29 21:42:02 +0200

    updated user action component to display amount of users dynamically

--- client/src/components/adminDashboard/MemberActions/MemberActions.jsx
+++ client/src/components/adminDashboard/MemberActions/MemberActions.jsx
@@ -1,12 +1,15 @@
 import { Button } from "@components";
+import { useUserContext } from "@hooks/useUserContext";
 import "./memberActions.scss";
 
 const MemberActions = ({ setCreateUserMenuOpen }) => {
+    const { users } = useUserContext();
+
     return (
         <section className="member-actions">
             <div className="member-actions__title">
                 <h3>
-                    Leden (<span className="member-actions__total-amount">56</span>)
+                    Leden (<span className="member-actions__total-amount">{users.length}</span>)
                 </h3>
             </div>
             <div className="member-actions__btns">
